0b4a98cadd4ccabef12def1c61ff75f69d356019
Julian <julianxchang@gmail.com>
2025-11-23 17:32:41 -0800

    update scheduler to ping heartbeat

--- scheduler.py
+++ scheduler.py
@@ -1,16 +1,22 @@
-import time
-import subprocess
-import os
+import time, subprocess, os, sys, requests
 from dotenv import load_dotenv
-import sys
 
 load_dotenv()
+
+HEARTBEAT_URL = os.getenv("HEARTBEAT_URL")
 
 def run_requests():
     i = 0
     while True:
         print(f"Starting course check #{i+1}...")
         subprocess.run([sys.executable, "worker.py"])
+
+        try:
+            requests.get(HEARTBEAT_URL, timeout=5)
+            print("Heartbeat sent.")
+        except Exception as e:
+            print("Failed to send heartbeat:", e)
+
         i += 1
         if os.getenv('ENV') == 'prod':
             time.sleep(120)  # check every 2 minutes
